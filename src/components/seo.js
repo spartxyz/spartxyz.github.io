22e210fa9c12c363ed2c52ce0dafda7b571daa18
joe <joeczepil@gmail.com>
2020-02-01 18:02:36 -0800

    Fix meta?

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 
-function SEO({ description, lang, meta, title, keywords }) {
+function SEO({ description, lang, meta, title }) {
   return (
     <Helmet
       htmlAttributes={{
@@ -34,9 +34,11 @@ function SEO({ description, lang, meta, title, keywords }) {
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
+  description: ``,
 }
 
 SEO.propTypes = {
+  description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
